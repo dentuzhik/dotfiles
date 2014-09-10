4837d4b42ab800fdd20b694891bb4d866e6857fb
Denis Tuzhik <dentuzhik@gmail.com>
2014-09-10 12:36:35 +0400

    Add tmux configuration file. Upgrade boostrap script.
    Make bootstrap script more user-friendly.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,25 @@
 # Basic symlinking
-base_dir=$HOME/dotfiles
+function link {
+    base_dir=$HOME/dotfiles
+    files='.vimrc .tmux.conf .gitconfig .bash_aliases'
 
-ln -fs $base_dir/.vimrc $HOME/.vimrc
-ln -fs $base_dir/.gitconfig $HOME/.gitconfig
-ln -fs $base_dir/.bash_aliases $HOME/.bash_aliases
+    for file_name in $files; do
+        ln -fs $base_dir/$file_name $HOME/$file_name
+    done
+
+    echo 'Done.'
+}
+
+echo 'This script may overwrite some files in your $HOME';
+echo 'Do you want to continue [y/n]?'
+
+read yn;
+case $yn in
+    'y')
+        link
+    ;;
+    'n')
+        echo 'Aborted.'
+    ;;
+esac
+
