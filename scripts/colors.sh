accb26d9a3eaaed71263e217e17123b449c5c04f
Denis Tuzhik <dentuzhik@gmail.com>
2014-10-10 12:55:10 +0400

    Add more colored echos

--- scripts/colors.sh
+++ scripts/colors.sh
@@ -2,6 +2,22 @@ function echo_red() {
     echo -e "\e[1;31m$1\e[0;39m"
 }
 
+function echo_yellow() {
+    echo -e "\e[1;33m$1\e[0;39m"
+}
+
+function echo_blue() {
+    echo -e "\e[1;34m$1\e[0;39m"
+}
+
+function echo_cyan() {
+    echo -e "\e[1;36m$1\e[0;39m"
+}
+
+function echo_magenta() {
+    echo -e "\e[1;35m$1\e[0;39m"
+}
+
 function prompt_red() {
     echo "\[\e[1;31m\]$1\[\e[0;39m\]"
 }
