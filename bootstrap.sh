82841f8f2e2901285e216e4863c22211b94c59ef
Denis Tuzhik <dentuzhik@gmail.com>
2015-04-21 15:04:30 +0300

    Add .mongorc.js

--- bootstrap.sh
+++ bootstrap.sh
@@ -36,7 +36,7 @@ function link {
 
 : ${dotfiles_dir:=~/dotfiles}
 : ${base_dir:=~/dotfiles}
-: ${entries:='.bash_profile .bashrc .bash_aliases .vimrc .ssh/config .tmux.conf .gitconfig .editorconfig'}
+: ${entries:='.bash_profile .bashrc .bash_aliases .vimrc .ssh/config .tmux.conf .gitconfig .editorconfig .mongorc.js'}
 
 echo 'This script may overwrite some files in your $HOME'
 read -p 'Do you want to continue (y/n)? ' yn
