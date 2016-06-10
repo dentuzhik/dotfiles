83786cb9be8bda043c0b5f7be7c97e9df5b491c6
Denis Tuzhik <dentuzhik@gmail.com>
2016-06-11 00:35:37 +0300

    Modify chpwd script to ask for installation of missing node version

--- scripts/chpwd.sh
+++ scripts/chpwd.sh
@@ -1,6 +1,21 @@
 function chpwd() {
     if [[ -f $PWD/.nvmrc && -r $PWD/.nvmrc ]]; then
-        nvm use
+        local nvm_error=$(nvm use 2>&1 > /dev/null)
+        if [[ ! -z $nvm_error ]]; then
+            # Extracting missing version from error string
+            local missing_version=$(expr "$nvm_error" : ".*\(v[0-9]\(\.[0-9]\)\{1,2\}\)")
+            echo $nvm_error
+            read -p "Do you want install version $missing_version (y/n)? " yn
+
+            case $yn in
+                'y')
+                    nvm install $missing_version
+                ;;
+                'n')
+                    return
+                ;;
+            esac
+        fi
     elif [[ $(nvm version) != $(nvm version default)  ]]; then
         nvm use default
     fi
