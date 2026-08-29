34abbf4ae75ac201fdfba4f8cbad4201a5e34ed6
Denis Tuzhik <dev@dentuzhik.com>
2026-08-29 09:42:51 +0300

    Modernize browser routing and remove organization data

--- .finicky.js
+++ .finicky.js
@@ -1,135 +1,47 @@
+const trackingParameters = [
+    "fbclid",
+    "gclid",
+    "dclid",
+    "msclkid",
+    "mc_cid",
+    "mc_eid",
+];
+
+// CommonJS keeps this configuration compatible with Finicky 3 and 4. Switch to
+// `export default` after every machine has upgraded to Finicky 4.
 module.exports = {
-    defaultBrowser: "Browserosaurus",
-    // https://github.com/johnste/finicky/wiki/Configuration#general-options
-    options: {
-        hideIcon: true,
-    },
+    defaultBrowser: "Arc",
     rewrite: [
         {
-            match: () => true, // Execute rewrite on all incoming urls to make this example easier to understand
-            url({ url }) {
-                const removeKeysStartingWith = ["utm_", "uta_"]; // Remove all query parameters beginning with these strings
-                const removeKeys = ["fblid", "gclid"]; // Remove all query parameters matching these keys
+            match: () => true,
+            url: ({ url }) => {
+                const searchParams = new URLSearchParams(url.search);
 
-                const search = url.search
-                    .split("&")
-                    .map((parameter) => parameter.split("="))
-                    .filter(
-                        ([key]) =>
-                            !removeKeysStartingWith.some((startingWith) =>
-                                key.startsWith(startingWith)
-                            )
-                    )
-                    .filter(
-                        ([key]) =>
-                            !removeKeys.some((removeKey) => key === removeKey)
-                    );
+                for (const key of [...searchParams.keys()]) {
+                    if (
+                        key.startsWith("utm_") ||
+                        key.startsWith("uta_") ||
+                        trackingParameters.includes(key)
+                    ) {
+                        searchParams.delete(key);
+                    }
+                }
 
                 return {
                     ...url,
-                    search: search
-                        .map((parameter) => parameter.join("="))
-                        .join("&"),
+                    search: searchParams.toString(),
                 };
-            },
-        },
-        {
-            // Replace domain of urls to amazon.com with smile.amazon.com
-            match: "amazon.com/*",
-            url: {
-                host: "smile.amazon.com",
             },
         },
     ],
     handlers: [
         {
-          // Redirect all web links
-          // from: https://app.slack.com/client/<team id>/<channel>
-          // to: slack://channel?team=<team-id>&id=<channel-id>
-          //
-          // Redirect all deep linked messages
-          // from: https://<subdomain>.slack.com/archives/<channel-id>/p<16-digit-timestamp>
-          // to: slack://channel?team=<team-id>&id=<channel-id>&message=<10-digit-6-decimal-timestamp>
-          browser: "/Applications/Slack.app",
-          match: [
-            '*.slack.com/client/*',
-            '*.slack.com/archives/*'
-          ],
-          url({ url }) {
-            const parts = url.pathname.split('/')
-            // Return input URL if no expected path is found
-            if (parts.length < 2) return url
-            let team
-            switch (parts[1]) {
-              // For direct web links
-              case 'client':
-                team = parts[2]
-                parts.splice(2, 1) // Remove team identifier to match archives format
-                break
-              // For deep links
-              case 'archives':
-                const org = url.host.split('.')[0]
-                switch (org) {
-                  case 'taxify':
-                    // Starts with a T and can be found in the web app URL for any channel in your org
-                    team = 'T02SA4XL8'
-                    break
-                  default:
-                    // Return input URL if no team lookup available
-                    return url
-                  }
-            }
-            search = `team=${team}`
-            let channel = parts[2]
-            if (parts.length === 3) {
-            // If this is a link to a channel/user
-              search = `${search}&id=${channel}`
-            // If this is a link to a message
-            } else if (parts.length === 4) {
-              const message = parts[3].slice(1, 11) + '.' + parts[3].slice(11)
-              search = `${search}&channel=${channel}&message=${message}`
-            }
-            return {
-                protocol: "slack",
-                username: "",
-                password: "",
-                host: "channel",
-                port: null,
-                pathname: "",
-                search: search,
-                hash: ""
-            }
-          }
-        },
-        // https://github.com/johnste/finicky/wiki/Configuration-ideas#open-spotify-links-in-spotify-app
-        {
             match: "open.spotify.com/*",
             browser: "Spotify",
         },
         {
-            match: "https://www.figma.com/file/*",
+            match: "www.figma.com/*",
             browser: "Figma",
-        },
-        {
-            // Open apple.com and example.org urls in Safari
-            match: finicky.matchHostnames([
-                "localhost",
-                "github.com",
-                "taxify.atlassian.net",
-                "bolt.zoom.us",
-                "small-improvements.com",
-                "taxify.slack.com",
-                "docs.google.com",
-                "app2.greenhouse.io",
-            ]),
-            browser: "Arc",
-        },
-        {
-            // Open any link clicked in Slack in Safari
-            match: ({ sourceBundleIdentifier }) =>
-                sourceBundleIdentifier === "com.tinyspeck.slackmacgap" ||
-                sourceBundleIdentifier === "com.googlecode.iterm2",
-            browser: "Arc",
         },
     ],
 };
